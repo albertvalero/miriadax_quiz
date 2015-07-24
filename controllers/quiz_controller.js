369398d8a983b4d122ed9f287a40b834040d496b
albertvalero <albertvalero@gmail.com>
2015-07-25 00:48:12 +0100

    Primera pregunta mejorado

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,10 @@
+// Get /quizes/question
+
+exports.question = function(req, res){
+	res.render('/quizes/question', {pregunta: "Cual es la capital de Italia"});
+};
+
+exports.answer = function(req, res){
+	if (req.query.respuesta === "Roma") res.render('/quizes/answer', {respuesta: "Correcto"});
+	else res.render('/quizes/answer', {respuesta: "Incorrecto"});
+};
